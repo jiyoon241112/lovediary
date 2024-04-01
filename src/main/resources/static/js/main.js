2e91f3e14ea96c22ce9bad9c1fe156397a6c225c
jiyoonJung116 <141412977+jiyoonJung116@users.noreply.github.com>
2024-04-01 17:17:58 +0900

    채팅 스타일 수정

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,54 @@
+// 프로필 수정 페이지
+$("#profile_btn").click(function() {
+    location.href = "/profile";
+});
+
+// 알람 페이지
+$("#alarm_btn").click(function() {
+    location.href = "/alarm";
+});
+
+// 커뮤니티 페이지
+$("#community_btn").click(function() {
+    location.href = "/community";
+});
+
+// 가계부 페이지
+$("#household_ledger_btn").click(function() {
+    location.href = "/household";
+});
+
+// 데이트 장소/맛집 페이지
+$("#place_btn").click(function() {
+    location.href = "/place";
+});
+
+// 스토어 페이지
+$("#store_btn").click(function() {
+    location.href = "/store";
+});
+
+// 오늘의 질문 페이지
+$("#question_btn").click(function() {
+    location.href = "/question/regist";
+});
+
+// 일정 페이지
+$("#calendar_btn").click(function() {
+    location.href = "/schedule";
+});
+
+// 드라이브 페이지
+$("#drive_btn").click(function() {
+    location.href = "/drive";
+});
+
+// 일기 페이지
+$("#diary_btn").click(function() {
+    location.href = "/diary";
+});
+
+// 채팅 페이지
+$("#chat_btn").click(function() {
+    location.href = "/chatting";
+});
