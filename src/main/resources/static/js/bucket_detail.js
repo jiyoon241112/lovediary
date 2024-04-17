27a076d9cc98109ef2a720f26183303e8daa15b1
jjy <mpkung116@gmail.com>
2024-04-17 19:08:18 +0900

    버킷리스트 삭제 개발

--- src/main/resources/static/js/bucket_detail.js
+++ src/main/resources/static/js/bucket_detail.js
@@ -20,7 +20,37 @@ $(".add_btn").click(function (){
     location.href = "/bucket/item?bucket_idx=" + idx;
 });
 
-$(".disable_btn").click(function(){
+$("#return_bucket").click(function(){
    location.href = "/bucket";
 });
+
+$("#delete_btn").click(function (){
+    let idx = $(this).closest(".comment.card").data("idx");
+    let form_data = new FormData;
+    form_data.append("idx", idx);
+
+    deleteBucket(form_data);
+});
+
+function deleteBucket(form_data, retry = false) {
+    $.ajax({
+        url: '/bucket/delete',
+        method: 'post',
+        data : form_data,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            const msg = data.msg ?? null;
+            if(msg ?? null) {
+                alert(msg);
+            }
+
+            if(data.code === "200") {
+                location.replace("/bucket");
+            }
+        }, error: function () {
+            if(!retry) save(form_data, true);
+        }
+    });
+}
 
